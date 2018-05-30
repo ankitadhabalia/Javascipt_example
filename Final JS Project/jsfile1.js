546f9e130e5bf233591c343df3a99ae9ec715b79
Ankita Dhabalia <ankitadhabalia@maihar.com>
2018-05-30 15:55:32 +0530

    Javascript_validation

---
+++ Final JS Project/jsfile1.js
@@ -0,0 +1,51 @@
+﻿
+/*page 1*/
+
+function login_successful_or_not() {
+    var entered_value1, entered_value2;
+    entered_value1 = document.getElementById("Text1").value;
+    entered_value2 = document.getElementById("Password1").value;
+
+    if (entered_value1 == "ankitadhabalia@gmail.com" && entered_value2 == "ankita@3") {
+        alert("Login Successful");
+        return true;
+    } else {
+        alert("Login Unsuccessful");
+        return false;
+    }
+}
+
+/*page 2*/
+
+function confirmLogout() {
+    if (confirm("Are you sure you wish to logout?")) {
+        window.location = "login_form1.html";
+    } else {
+        return false;
+    }
+}
+
+/*page 3*/
+
+var obj1 = { "Name": "ANKITA", "Email": "ankitadhabalia@gmail.com", "Phone": "8149214150", "Address": "Telephone Exchange Sqaure" };
+var text1 = JSON.stringify(obj1);
+localStorage.setItem("key", text1);
+
+var text2 = localStorage.getItem('key');
+var obj2 = JSON.parse(text2);
+document.getElementById("name").value = obj2.Name;
+document.getElementById("email").value = obj2.Email;
+document.getElementById("phone").value = obj2.Phone;
+document.getElementById("address").value = obj2.Address;
+
+/* Page 4 */
+
+/*
+function entry_submit() {
+    document.getElementById("text1").innerHTML = document.getElementById("n").value;
+    document.getElementById("text2").innerHTML = document.getElementById("n").value;
+    document.getElementById("text3").innerHTML = document.getElementById("n.").value;
+    document.getElementById("text4").innerHTML = document.getElementById("n").value;
+
+}
+ */
---
+++ Final JS Project/local_storage3.js
@@ -0,0 +1,23 @@
+﻿
+
+var myObj = { "Name": "ANKITA", "Email": "ankitadhabalia@gmail.com", "Phone": "8149214150", "Address" : "Telephone Exchange Sqaure" };
+
+var myJSON = JSON.stringify(myObj);
+
+localStorage.setItem("testJSON", "myJSON");
+
+var text = localStorage.getItem("testJSON");
+
+var obj = JSON.parse(text);
+document.getElementById("result").innerHTML = obj.Name;
+
+/*
+localStorage.setItem("Email", "ankitadhabalia@gmail.com");
+document.getElementById("result").innerHTML = localStorage.getItem("Email");
+
+localStorage.setItem("Phone", "8149214150");
+document.getElementById("result").innerHTML = localStorage.getItem("8149214150");
+
+localStorage.setItem("Address", "Telephone Exchange Sqaure");
+document.getElementById("result").innerHTML = localStorage.getItem("Telephone Exchange Sqaure");
+*/
